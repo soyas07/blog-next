c2172ebc74e14f9fd3e5b0275fd6c6cc4d9a7451
Soyas Limbu <90306699+soyas07@users.noreply.github.com>
2023-02-11 23:38:29 +1100

    Fixes #9

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styles from './blog.module.css';
 
 const Blog = () => {
     return (
-        <div>Blog</div>
+        <div class={`${styles.blogContainer} main-container`}>
+            <div class={styles.header}></div>
+            <div class={styles.article}></div>
+            <div class={styles.recipes}></div>
+            <div class={styles.pagination}></div>
+            <div class={styles.subscribe}></div>
+        </div>
     )
 }
 
